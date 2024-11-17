81d4df339ed5da55a362bb389607e1e575d0f00b
Maxim Nedobezhkin <m.nedobezhkin@astondevs.ru>
2024-11-17 20:02:58 +0700

    рефактор корс конфига

--- src/main/java/com/dlb/lemon_bank/config/CorsConfig.java
+++ src/main/java/com/dlb/lemon_bank/config/CorsConfig.java
@@ -1,8 +1,10 @@
 package com.dlb.lemon_bank.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
